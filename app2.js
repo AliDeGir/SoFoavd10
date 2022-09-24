885f6662534edb0ccad55f048a7d4753b199c481
AliDeGir <ali_deg@hotmail.com>
2022-09-24 14:19:46 +0200

    First commit

---
+++ app2.js
@@ -0,0 +1,19 @@
+function checkPassword() {
+    if (document.getElementById('PASSWORD').value == 123) {
+        document.getElementById('HIDDEN-CONTAINER').classList.remove("hidden");
+        document.getElementById('FORM-POPUP').style.visibility= "hidden"
+        document.getElementById('VIP-BTN').style.visibility= "hidden"
+    } else {
+        document.getElementById('PASSWORD').value= ""; // RESETTING THE PASSWORD AFTER FAIL
+        alert('Invalid Password!!'); password.setSelectionRange(0, password.value.length);
+    }
+}
+function openForm() {
+    document.getElementById('FORM-POPUP').style.visibility= "visible";
+}
+function closeForm() {
+    document.getElementById('FORM-POPUP').style.visibility= "hidden";
+}
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
