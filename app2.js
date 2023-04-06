fd2369a785c97a6ffc47f0d34b2ecd261fc36011
AliDeGir <ali_deg@hotmail.com>
2023-04-06 18:55:45 +0200

    click img function

--- app2.js
+++ app2.js
@@ -28,4 +28,19 @@ imgs.forEach(img => {
   });
 });
 
-// testing
+function openFullscreen() {
+    var img = event.target;
+    var fullScreenDiv = document.createElement("div");
+    fullScreenDiv.classList.add("fullscreen");
+    fullScreenDiv.onclick = closeFullscreen;
+    var imgClone = img.cloneNode(true);
+    fullScreenDiv.appendChild(imgClone);
+    document.body.appendChild(fullScreenDiv);
+    document.body.style.overflow = "hidden"; /* disable scrolling in background */
+}
+
+function closeFullscreen() {
+    var fullScreenDiv = document.querySelector(".fullscreen");
+    fullScreenDiv.parentNode.removeChild(fullScreenDiv);
+    document.body.style.overflow = "auto"; /* enable scrolling again */
+}
