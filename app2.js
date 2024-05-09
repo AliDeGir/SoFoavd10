9669b6b10c484479478a39eeb66013a0213a9af5
AliDeGir <ali_deg@hotmail.com>
2024-05-09 18:39:45 +0200

    added tariff calculator

--- app2.js
+++ app2.js
@@ -34,3 +34,36 @@ function closeFullscreen() {
     fullScreenDiv.parentNode.removeChild(fullScreenDiv);
     document.body.style.overflow = "auto"; /* enable scrolling again */
 }
+
+function calculateSalary() {
+  // Get the input values
+  const salary = parseFloat(document.getElementById("salary").value);
+  const year = document.getElementById("year").value;
+  const kontigent = salary * 0.019;
+
+  const savedOnTax = kontigent * 0.3;
+  // Perform any processing with the salary and chosen year
+  // For demonstration, let's say we simply multiply the salary by 1.1 (10% increase) if it's for 2024
+
+  let yearIncrease;
+
+  if (year === "2023") {
+    yearIncrease = 1700 * 10.5;
+  } else if (year === "2024") {
+    yearIncrease = 1700 * 7;
+  } else {
+    yearIncrease = "Invalid year"; // Just in case
+  }
+
+  let result = savedOnTax + yearIncrease - kontigent;
+
+  let html = `
+        <p>Betalt kontigent: ${kontigent.toFixed(2)} kr<br>
+        Skattefradrag på kontigent: ${savedOnTax.toFixed(2)} kr<br>
+        Økning på timelønn i ${year}: ${yearIncrease.toFixed(2)} kr<br><br>
+        Med medlemsskap går du i pluss minst:</p>
+        <h2>${result.toFixed(2)} kr</h2><br>
+        <p><i>Da er ikke økning i lokale lønnsforhandlinger inkludert i kalkulasjonen. Med flere medlemmer har vi mulighet å øke dette beløpet.</i></p>
+    `;
+  document.getElementById("result").innerHTML = html;
+}
